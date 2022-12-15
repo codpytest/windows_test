0fa97b0338efe076ffd240c19b59b998fb0476f8
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2022-12-15 17:31:33 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 ﻿# from __future__ import annotations
 from setuptools import Extension
 from setuptools import setup
+from Cython.Build import cythonize
 __version__ = '0.0.1'
 
 
 
-setup(ext_modules=[Extension(name="printf", sources=["src/printf.c"])])
+setup(ext_modules=cythonize([Extension(name="printf", sources=["src/printf.c"])]))
